ade1d25a731136a78bf41d46c62151d702c1acb3
matthieu rouvin <matthieu.rouvin@gmail.com>
2016-11-15 15:40:11 +0100

    task manager completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # ressources :tasks
-  # GET '/tasks': get all your tasks.
-  get "/tasks" => "tasks#index"
-  # GET '/tasks/:id': get a precise task, e.g GET '/tasks/33' get task with id=3
-  get "/tasks/:id" => "tasks#show"
-  # GET '/tasks/new': get the form to create a new task
-  get "/tasks/new" => "tasks#new"
-  # POST '/tasks': post a new task
-  post "/tasks" => "tasks#create"
-  # GET '/tasks/:id/edit': get the form to edit an existing task
-  get "/tasks/:id/edit" => "tasks#edit"
-  # PATCH '/tasks/:id': update an existing task
-  patch "/tasks/:id" => "tasks#update"
-  # DELETE '/tasks/:id': delete an existing task
-  delete "/tasks/:id" => "tasks#destroy"
-  # Naming conventions
-  #   index
-  #   show
-  #   new
-  #   create
-  #   edit
-  #   update
-  #   destroy
+
+  # get "/tasks", to: "tasks#index"
+
+  # get "/tasks/new", to: "tasks#new"
+
+  # post "/tasks", to: "tasks#create"
+
+  # get "/tasks/:id", to: "tasks#show"
+
+  # get "/tasks/:id/edit", to: "tasks#edit"
+
+  # patch "/tasks/:id", to: "tasks#update"
+
+  # delete "/tasks/:id", to: "tasks#destroy"
+
+
+  resources :tasks
+
 end
